28c75115ed31a93a926f37cb05e0ad83183d41fc
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-07-09 17:12:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    public static void main(String[] args) {
+        Semaphore sem = new Semaphore(1); // 1 разрешение
+        CommonResource res = new CommonResource();
+        new Thread(new CountThread(res, sem, "CountThread 1")).start();
+        new Thread(new CountThread(res, sem, "CountThread 2")).start();
+        new Thread(new CountThread(res, sem, "CountThread 3")).start();
+    }
+}
